b237d3351b2542ca439adc0e210a9b7b5642bb59
IttoSanzzo <konohittos@gmail.com>
2026-07-08 01:13:12 -0300

    Added PlaylistVideo as a supported video type for the playlist loader

--- src/app/api/youtube/playlist/categories/route.ts
+++ src/app/api/youtube/playlist/categories/route.ts
@@ -12,14 +12,30 @@ export async function GET(req: NextRequest) {
 	if (!playlistId) return;
 
 	const playlist = await youtubeApi.getPlaylist(playlistId);
-	const tracks: TrackInfo[] = playlist.videos.map((video: LintIgnoredAny) => ({
-		name: video.metadata.title.text ?? "",
-		description: "",
-		link: video.content_id
-			? `https://www.youtube.com/watch?v=${video.content_id}`
-			: "",
-		artworkUrl: video.content_image.image[0].url,
-	}));
+
+	const tracks: TrackInfo[] = playlist.videos.map((video: LintIgnoredAny) => {
+		switch (video.type) {
+			case "LockupView":
+				return {
+					name: video.metadata.title.text ?? "",
+					description: "",
+					link: video.content_id
+						? `https://www.youtube.com/watch?v=${video.content_id}`
+						: "",
+					artworkUrl: video.content_image.image[0].url,
+				};
+			case "PlaylistVideo":
+			default:
+				return {
+					name: video.title.text ?? "",
+					description: "",
+					link: video.endpoint.payload.videoId
+						? `https://www.youtube.com/watch?v=${video.endpoint.payload.videoId}`
+						: "",
+					artworkUrl: video.thumbnails[0].url,
+				};
+		}
+	});
 	const category: TrackCategory = {
 		title: playlist.info.title ?? "Playlist",
 		tracks,
