4eb6b8ad24348847a08aefa98e056925b34cb309
IttoSanzzo <konohittos@gmail.com>
2026-07-15 18:42:06 -0300

    Remove from queue and a link button where added to the queue panel

--- src/app/(site)/embeds/[targetBot]/[guildId]/queue/pageContent.tsx
+++ src/app/(site)/embeds/[targetBot]/[guildId]/queue/pageContent.tsx
@@ -14,10 +14,14 @@ const QueueContainer = newStyledElement.div(styles.queueContainer);
 const QueueTitle = newStyledElement.div(styles.queueTitle);
 const NoTracksMessage = newStyledElement.p(styles.noTracksMessage);
 const TracksList = newStyledElement.div(styles.tracksList);
+const TrackEntryContainer = newStyledElement.div(styles.trackEntryContainer);
 const TrackEntry = newStyledElement.button(styles.trackEntry);
 const QueueEnd = newStyledElement.div(styles.queueEnd);
 const IndexIcon = newStyledElement.div(styles.indexIcon);
-const PlataformIcon = newStyledElement.div(styles.plataformIcon);
+const PlataformIconLink = newStyledElement.a(styles.plataformIconLink);
+const RemoveTrackEntryButton = newStyledElement.button(
+	styles.removeTrackEntryButton,
+);
 const ArtworkPreview = newStyledElement.div(styles.artworkPreview);
 const Title = newStyledElement.p(styles.title);
 
@@ -63,7 +67,7 @@ export default function Queue({
 		}, 1500);
 	}, []);
 
-	async function handleClick(position: number) {
+	async function handlePlayTrack(position: number) {
 		if (!queue) return;
 		const toastId = toast.loading("Play");
 		try {
@@ -91,6 +95,34 @@ export default function Queue({
 			});
 		}
 	}
+	async function handleTrackRemoval(position: number) {
+		if (!queue) return;
+		const toastId = toast.loading("Remove");
+		try {
+			const response = await fetch(
+				`/api/${targetBot}/${queue.guildId}/action`,
+				{
+					method: "POST",
+					body: JSON.stringify({
+						userId,
+						action: "Remove",
+						trackPosition: position,
+					}),
+					headers: {
+						"Content-Type": "application/json",
+					},
+				},
+			);
+			toast[response.ok ? "success" : "error"]("Remove", {
+				id: toastId,
+			});
+		} catch {
+			console.error("Exception");
+			toast.error(`Remove Command Exception`, {
+				id: toastId,
+			});
+		}
+	}
 
 	if (!queue)
 		return (
@@ -110,42 +142,51 @@ export default function Queue({
 						const plataformType = getPlataformType(track.link);
 
 						return (
-							<TrackEntry
-								onClick={userId ? () => handleClick(index + 1) : undefined}
-								key={`${index}:${track.link}`}
-								className={
-									index + 1 == queue.currentIndex
-										? clsx(
-												styles.current,
-												queue.isPaused ? styles.paused : undefined,
-												queue.loopState == 1 ? styles.trackLoop : undefined,
-												queue.loopState == 2 ? styles.queueLoop : undefined,
-											)
-										: undefined
-								}
-								ref={
-									index + 1 == queue.currentIndex ? currentTrackRef : undefined
-								}
-								title={`${track.title}\n\n${track.link}`}>
-								<ArtworkPreview>
-									<Image
-										src={track.artwork}
-										alt={"Track's artwork"}
-										fill
-									/>
-								</ArtworkPreview>
-								<IndexIcon>
-									{index + 1}
-									<PlataformIcon>
+							<TrackEntryContainer key={`${index}:${track.link}`}>
+								<TrackEntry
+									onClick={
+										userId ? () => handlePlayTrack(index + 1) : undefined
+									}
+									className={
+										index + 1 == queue.currentIndex
+											? clsx(
+													styles.current,
+													queue.isPaused ? styles.paused : undefined,
+													queue.loopState == 1 ? styles.trackLoop : undefined,
+													queue.loopState == 2 ? styles.queueLoop : undefined,
+												)
+											: undefined
+									}
+									ref={
+										index + 1 == queue.currentIndex
+											? currentTrackRef
+											: undefined
+									}
+									title={`${track.title}\n\n${track.link}`}>
+									<ArtworkPreview>
 										<Image
-											src={plataformIcons[plataformType]}
-											alt={""}
+											src={track.artwork}
+											alt={"Track's artwork"}
 											fill
 										/>
-									</PlataformIcon>
-								</IndexIcon>
-								<Title>{track.title}</Title>
-							</TrackEntry>
+									</ArtworkPreview>
+									<IndexIcon>{index + 1}</IndexIcon>
+									<Title>{track.title}</Title>
+								</TrackEntry>
+								<PlataformIconLink
+									href={track.link}
+									target="_blank">
+									<Image
+										src={plataformIcons[plataformType]}
+										alt={""}
+										fill
+									/>
+								</PlataformIconLink>
+								<RemoveTrackEntryButton
+									onClick={() => handleTrackRemoval(index + 1)}>
+									X
+								</RemoveTrackEntryButton>
+							</TrackEntryContainer>
 						);
 					})}
 					<QueueEnd>{withEndMessage && "End"}</QueueEnd>
